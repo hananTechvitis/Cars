9f2d63b870d0674fa6f0e18aa4df638637080c9a
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-27 13:06:34 +0200

    1

--- myproject/BigCar/autocar/urls.py
+++ myproject/BigCar/autocar/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import hello
+from . import views
 
 urlpatterns = [
-    path('/', hello, name='hello'),
+    path('hello/', views.hello, name='hello'),
 ]
