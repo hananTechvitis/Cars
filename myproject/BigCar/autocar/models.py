578cb232db692cc1ed9ace18c078a7f189614aa3
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-27 16:18:15 +0200

    1

--- myproject/BigCar/autocar/models.py
+++ myproject/BigCar/autocar/models.py
@@ -9,3 +9,6 @@ class Car(models.Model):
     year = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='car_images')
+
+def __str__(self):
+        return f"{self.brand.name} {self.model} ({self.year})"
