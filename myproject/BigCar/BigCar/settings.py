e3b3d9060b1b5e0d4c1b11ab7dd092a539fe9ed0
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-27 14:16:55 +0200

    1

--- myproject/BigCar/BigCar/settings.py
+++ myproject/BigCar/BigCar/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'BigCar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': '18.199.168.226',
+        'PORT': '5432',
     }
 }
 
