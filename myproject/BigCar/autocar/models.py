130e99982d225109d152957e9409ee7dfc33b38a
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-27 12:51:48 +0200

    5

--- myproject/BigCar/autocar/models.py
+++ myproject/BigCar/autocar/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-
-class Brand(models.Model):
-    name = models.CharField(max_length=100)
-
-class Car(models.Model):
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    model = models.CharField(max_length=100)
-    year = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='car_images')
