1d9f32854b931f013e7e8c9b1983584e93103daa
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-27 13:36:40 +0200

    1

--- myproject/BigCar/autocar/views.py
+++ myproject/BigCar/autocar/views.py
@@ -6,8 +6,8 @@ def hello(request):
     return HttpResponse("Hello, world! This is my first Django view.")
 
 def home(request):
-    return render(request, 'frontend/home.html')
+    return render(request, 'home.html')
 
 def car_list(request):
     cars = Car.objects.all()
-    return render(request, 'frontend/cars.html', {'cars': cars})
+    return render(request, 'cars.html', {'cars': cars})
