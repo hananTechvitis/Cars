98a057fcc55c488ec61e94a06ea8ace562b60b0f
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-27 16:33:41 +0200

    1

--- myproject/BigCar/autocar/models.py
+++ myproject/BigCar/autocar/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Brand(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     model = models.CharField(max_length=100)
@@ -10,5 +13,5 @@ class Car(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='car_images')
 
-def __str__(self):
-        return self.title
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
