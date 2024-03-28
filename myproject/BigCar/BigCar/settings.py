e3c3aa1cc6c5b2c7f93b8261a1586e494e67ae16
hananTechvitis <hanan.eltassa@techvitis.com>
2024-03-28 11:41:48 +0200

    q

--- myproject/BigCar/BigCar/settings.py
+++ myproject/BigCar/BigCar/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'autocar',
+    
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = ['django.contrib.staticfiles',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
