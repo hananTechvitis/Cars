1bbdbe886e715d2a5e2a56a9adbf47c41b7e1263
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-02 11:46:58 +0300

    image

--- myproject/BigCar/BigCar/settings.py
+++ myproject/BigCar/BigCar/settings.py
@@ -139,6 +139,6 @@ STATICFILES_DIRS = [
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- myproject/BigCar/autocar/urls.py
+++ myproject/BigCar/autocar/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -9,9 +8,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cars/', views.car_list, name='cars'),
     path('image/', views.image_view, name='image-view'),
-
 ]
-
-# This is only needed when you are in development mode
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
