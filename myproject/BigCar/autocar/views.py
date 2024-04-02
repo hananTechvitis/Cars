a6b34a74ffb8e86a9623530bd8502eb52a590607
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-02 09:40:10 +0300

    backend_1

--- myproject/BigCar/autocar/views.py
+++ myproject/BigCar/autocar/views.py
@@ -6,7 +6,14 @@ def hello(request):
     return HttpResponse("Hello, world! This is my first Django view.")
 
 def home(request):
-    return render(request, 'home.html')
+    # Fetch the last 5 cars added to the database
+    featured_cars = Car.objects.all().order_by('-id')[:5]
+    # Pass the cars to the template
+    return render(request, 'home.html', {'featured_cars': featured_cars})
+   
+
+
+
 
 def car_list(request):
     cars = Car.objects.all()
