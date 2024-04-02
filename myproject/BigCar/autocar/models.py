14dd117afdbd3a088d36873a6cedb6ee8ba63bd3
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-02 12:44:42 +0300

    new fields

--- myproject/BigCar/autocar/models.py
+++ myproject/BigCar/autocar/models.py
@@ -13,5 +13,15 @@ class Car(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='car_images')
 
+    # New fields
+    passengers = models.IntegerField(verbose_name='Number of Passengers')
+    TYPE_CHOICES = (
+        ('Hybrid', 'Hybrid'),
+        ('Gasoline', 'Gasoline'),
+    )
+    type = models.CharField(max_length=8, choices=TYPE_CHOICES, default='Gasoline', verbose_name='Car Type')
+    is_automatic = models.BooleanField(default=True, verbose_name='Is Automatic')
+    fuel_efficiency = models.CharField(max_length=15, verbose_name='Fuel Efficiency (km/litre)')
+    
     def __str__(self):
         return f"{self.brand} {self.model} ({self.year})"
