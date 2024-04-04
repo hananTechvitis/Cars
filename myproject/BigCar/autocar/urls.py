3ece4f3a5dbcaab3bd5e8475da6a0d4700a58f1f
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-04 10:06:47 +0300

    ss

--- myproject/BigCar/autocar/urls.py
+++ myproject/BigCar/autocar/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cars/', views.car_list, name='cars'),
     path('image/', views.image_view, name='image-view'),
-    path('rent-car/<int:car_id>/', views.rent_car, name='rent-car'),
+    
     path('payment/<int:car_id>/', views.payment, name='payment'),
 ]
