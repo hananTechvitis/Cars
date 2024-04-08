b9dbf1b6b94c60d73419c0843b0ac51965d428db
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-08 08:49:59 +0300

    static

--- myproject/BigCar/BigCar/settings.py
+++ myproject/BigCar/BigCar/settings.py
@@ -123,13 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 # Add this line if not already present
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
