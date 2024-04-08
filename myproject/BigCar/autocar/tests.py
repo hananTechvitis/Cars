5ee7f43efd3b8b9a2fc7b856b471886316b62774
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-08 14:23:36 +0300

    test

--- myproject/BigCar/autocar/tests.py
+++ myproject/BigCar/autocar/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import MyModel
 
-# Create your tests here.
+class MyModelTestCase(TestCase):
+    def setUp(self):
+        # Set up any necessary data or state before running each test method
+        self.my_instance = MyModel.objects.create(name='Test Name', description='Test Description')
+
+    def test_my_model_creation(self):
+        # Test if the MyModel object was created successfully
+        self.assertEqual(self.my_instance.name, 'Test Name')
+        self.assertEqual(self.my_instance.description, 'Test Description')
+
