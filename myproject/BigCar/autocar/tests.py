bb6902779f56a395b67cf81e044b2a8cd08493d5
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-08 14:28:37 +0300

    test

--- myproject/BigCar/autocar/tests.py
+++ myproject/BigCar/autocar/tests.py
@@ -1,13 +1,27 @@
 from django.test import TestCase
-from .models import MyModel
+from .models import Brand, Car
 
-class MyModelTestCase(TestCase):
+class CarModelTestCase(TestCase):
     def setUp(self):
         # Set up any necessary data or state before running each test method
-        self.my_instance = MyModel.objects.create(name='Test Name', description='Test Description')
+        self.brand = Brand.objects.create(name='Toyota')
+        self.car = Car.objects.create(
+            brand=self.brand,
+            model='Camry',
+            year=2022,
+            price=25000.00,
+            passengers=5,
+            type='Gasoline',
+            is_automatic=True,
+            fuel_efficiency='12'
+        )
 
-    def test_my_model_creation(self):
-        # Test if the MyModel object was created successfully
-        self.assertEqual(self.my_instance.name, 'Test Name')
-        self.assertEqual(self.my_instance.description, 'Test Description')
-
+    def test_car_model_creation(self):
+        # Test if the Car object was created successfully
+        self.assertEqual(self.car.model, 'Camry')
+        self.assertEqual(self.car.year, 2022)
+        self.assertEqual(self.car.price, 25000.00)
+        self.assertEqual(self.car.passengers, 5)
+        self.assertEqual(self.car.type, 'Gasoline')
+        self.assertTrue(self.car.is_automatic)
+        self.assertEqual(self.car.fuel_efficiency, '12')
