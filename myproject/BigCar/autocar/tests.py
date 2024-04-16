d44b513f30f623e5f946d765c87b2ffc58431fc7
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-16 18:48:47 +0300

    hanan

--- myproject/BigCar/autocar/tests.py
+++ myproject/BigCar/autocar/tests.py
@@ -1,27 +1,21 @@
 from django.test import TestCase
 from .models import Brand, Car
 from django.core.files.uploadedfile import SimpleUploadedFile
-import os
+from unittest.mock import mock_open, patch
 
 class CarModelTestCase(TestCase):
     def setUp(self):
         # Set up any necessary data or state before running each test method
         self.brand = Brand.objects.create(name='Toyota')
 
-        # Creating a dummy image for testing
-        image_path = os.path.join(os.path.dirname(__file__), 'media', 'car_images', 'R.png')
-        self.image = SimpleUploadedFile(
-            name='test_image.png',
-            content=open(image_path, 'rb').read(),
-            content_type='image/png'
-        )
+        
 
         self.car = Car.objects.create(
             brand=self.brand,
             model='Camry',
             year=2022,
             price=25000.00,
-            image=self.image,  # Attach the image to the model
+            image=self.image,  # Attach the mock image to the model
             passengers=5,
             type='Gasoline',
             is_automatic=True,
@@ -37,7 +31,7 @@ class CarModelTestCase(TestCase):
         self.assertEqual(self.car.type, 'Gasoline')
         self.assertTrue(self.car.is_automatic)
         self.assertEqual(self.car.fuel_efficiency, '12')
-        self.assertTrue(self.car.image)  # Test if image is attached successfully
+        self.assertTrue(self.car.image)  # Test if the mocked image is attached successfully
 
     def test_car_creation_and_list_view(self):
         # Test if the Car instance exists and its model attribute is correct
