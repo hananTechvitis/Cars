3727f32213a9827944566f656b5af06f4fe6da82
hananTechvitis <hanan.eltassa@techvitis.com>
2024-04-16 08:42:32 +0300

    111

--- myproject/BigCar/import_cars.py
+++ myproject/BigCar/import_cars.py
@@ -1,11 +1,20 @@
 import csv
 import os
 import django
+import requests
+from django.core.files.base import ContentFile
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BigCar.settings")
 django.setup()
 
 from autocar.models import Car, Brand
+
+def save_image_from_url(model_instance, url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_name = url.split('/')[-1]  # Extracts a basic filename from the URL
+        file_name = file_name.split('?')[0]  # Removes query parameters
+        model_instance.image.save(file_name, ContentFile(response.content), save=True)
 
 def import_cars_from_csv(csv_filename):
     with open(csv_filename, newline='', encoding='utf-8') as csvfile:
@@ -14,7 +23,6 @@ def import_cars_from_csv(csv_filename):
         for row in reader:
             brand_name = row['Brand']
             brand, _ = Brand.objects.get_or_create(name=brand_name)
-
             car, created = Car.objects.update_or_create(
                 brand=brand,
                 model=row['Model'],
@@ -29,6 +37,8 @@ def import_cars_from_csv(csv_filename):
             )
             if created:
                 cars_added += 1
+            if 'Image' in row and row['Image'].startswith('http'):
+                save_image_from_url(car, row['Image'])
         print(f"Successfully added {cars_added} new cars.")
 
 if __name__ == '__main__':
